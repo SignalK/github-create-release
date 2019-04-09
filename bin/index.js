36f352e6c0225e0934ed92bd2a0321f7af25c3aa
Scott Bender <scott@scottbender.net>
2019-04-09 15:53:02 -0400

    fix: catch and display errors

--- bin/index.js
+++ bin/index.js
@@ -33,42 +33,48 @@ const updateAll = argv['update-all']
 const npmBaseUrl = argv['npm-base-url']
 
 async function main() {
-  
-  const tags = await octokit.repos.listTags({owner, repo, per_page:!updateAll ? 2 : 100, pages:1 })
 
-  for ( let i = 0; i < tags.data.length; i++ ) {
-    const tag = tags.data[i]
-    if ( tag.name != 'latest' && i != 0 ) {
-      const prev = tags.data[i-1].name
-      const commits = await octokit.repos.compareCommits({owner, repo, base:tag.name, head:prev})
+  try
+  {
+    const tags = await octokit.repos.listTags({owner, repo, per_page:!updateAll ? 2 : 100, pages:1 })
 
-      const donePRs = []
-      let body = ''
-      for ( let j = 0; j < commits.data.commits.length; j++ ) {
-        const commit = commits.data.commits[j]
-        const prs = await octokit.search.issuesAndPullRequests({ q: `type:pr repo:${owner}/${repo} ${commit.sha}` })
-        if ( prs.data.items.length > 0 ) {
-          const pr = prs.data.items[0]
-          if ( donePRs.indexOf(pr.number) == -1 ) {
-            donePRs.push(pr.number)
-            body += `+ [#${pr.number}](${pr.pull_request.url}) ${pr.title} (@${pr.user.login})\n`
+    for ( let i = 0; i < tags.data.length; i++ ) {
+      const tag = tags.data[i]
+      if ( tag.name != 'latest' && i != 0 ) {
+        const prev = tags.data[i-1].name
+        const commits = await octokit.repos.compareCommits({owner, repo, base:tag.name, head:prev})
+
+        const donePRs = []
+        let body = ''
+        for ( let j = 0; j < commits.data.commits.length; j++ ) {
+          const commit = commits.data.commits[j]
+          const prs = await octokit.search.issuesAndPullRequests({ q: `type:pr repo:${owner}/${repo} ${commit.sha}` })
+          if ( prs.data.items.length > 0 ) {
+            const pr = prs.data.items[0]
+            if ( donePRs.indexOf(pr.number) == -1 ) {
+              donePRs.push(pr.number)
+              body += `+ [#${pr.number}](${pr.pull_request.url}) ${pr.title} (@${pr.user.login})\n`
+            }
           }
         }
-      }
 
-      const baseUrl = npmBaseUrl ? npmBaseUrl : `https://www.npmjs.com/package/@${owner}/${repo}`
-      body += `\n\n[This version in npm](${baseUrl}/v/${prev.slice(1)})`
-      try {
-        await octokit.repos.createRelease({owner, repo, tag_name: prev, body:body})
-      } catch ( e ) {
-        const release = await octokit.repos.getReleaseByTag({owner, repo, tag:prev})
-        await octokit.repos.updateRelease({owner, repo, release_id: release.data.id, prev, body:body})
-      }
-      console.log(`created or updated ${prev}`)
-      if ( !updateAll ) {
-        break
+        const baseUrl = npmBaseUrl ? npmBaseUrl : `https://www.npmjs.com/package/@${owner}/${repo}`
+        body += `\n\n[This version in npm](${baseUrl}/v/${prev.slice(1)})`
+        try {
+          await octokit.repos.createRelease({owner, repo, tag_name: prev, body:body})
+        } catch ( e ) {
+          const release = await octokit.repos.getReleaseByTag({owner, repo, tag:prev})
+          await octokit.repos.updateRelease({owner, repo, release_id: release.data.id, prev, body:body})
+        }
+        console.log(`created or updated ${prev}`)
+        if ( !updateAll ) {
+          break
+        }
       }
     }
+  } catch ( e ) {
+    console.error(e)
+    process.exit(1)
   }
 }
 
